1d0cef42f9f0154e2dab82aa76555cacaba50241
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-05-11 22:54:24 -0500

    Updated sentence parts to match subject types
    Sentence parts now match subject types to make a matching sentence.

--- script.js
+++ script.js
@@ -1,29 +1,90 @@
 const getSubject = () => {
-    const subjects = ["you", "a friend", "someone close to you", "a family member"];
+    const subjects = [
+        {
+            "part" : "You",
+            "type" : "you"
+        },
+        {
+            "part" : "A friend",
+            "type" : "other"
+        },
+        {
+            "part" : "Someone close to you",
+            "type" : "other"
+        },
+        {
+            "part" : "A family member",
+            "type" : "other"
+        }];
     
     let subjectIndex = Math.floor(Math.random() * subjects.length);
 
     return subjects[subjectIndex];
 }
 
-const getVerb = () => {
-    const verbs = ["will", "are about to"];
-    
-    let verbIndex = Math.floor(Math.random() * verbs.length);
+const getVerb = (subjectType) => {
+    const verbs = [
+        {
+            "part" : "will",
+            "type" : "any"
+        },
+        {
+            "part" : "are about to",
+            "type" : "you"
+        },
+        {
+            "part" : "is about to",
+            "type" : "other"
+        }];
+
+    let verbIndex = -1;
+    let typeMatch = false;
+
+    do {
+        verbIndex = Math.floor(Math.random() * verbs.length);
+        
+        if (verbs[verbIndex].type === subjectType || verbs[verbIndex].type === "any") {
+            typeMatch = true;
+        }
+    } while (!typeMatch);
 
     return verbs[verbIndex];
 }
 
-const getComplement = () => {
-    const complements = ["face a difficult time in your life", "have an unusual encounter soon"];
+const getComplement = (subjectType) => {
+    const complements = [
+        {
+            "part" : "face a difficult time in your life.",
+            "type" : "you"
+        },
+        {
+            "part" : "face a difficult time in their life.",
+            "type" : "other"
+        },
+        {
+            "part" : "have an unusual encounter soon.",
+            "type" : "any"
+        }];
     
-    let complementIndex = Math.floor(Math.random() * complements.length);
+    let complementIndex = -1;
+    let typeMatch = false;
+    
+    do {
+        complementIndex = Math.floor(Math.random() * complements.length);
+
+        if (complements[complementIndex].type === subjectType || complements[complementIndex].type === "any") {
+            typeMatch = true;
+        }
+    } while (!typeMatch);
 
     return complements[complementIndex];
 }
 
 const getFortune = () => {
 
-    let sentence = getSubject() + ' ' + getVerb() + ' ' + getComplement() + '.';
-    document.getElementById('fortune').innerText = sentence;
+    let subject = getSubject();
+    let verb = getVerb(subject.type);
+    let complement = getComplement(subject.type);
+
+    document.getElementById('fortune').innerText = `${subject.part} ${verb.part} ${complement.part}`;
 }
